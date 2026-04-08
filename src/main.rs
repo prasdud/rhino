0c2cdfad7fc7b2592a7cdda0f9b7fa02c1b5d1dc
muhammed-abdul-muid <muhammed.a.muid@houseofedtech.in>
2026-04-08 17:51:14 +0530

    added docker for local rhino_db, wip worker script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("welcome to rhino");
+use postgres::{Client, NoTls, Error};
+
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect("postgresql://rhino:rhino@localhost:5445/rhino_db", NoTls)?;
+    
+    let res = client.batch_execute("
+        CREATE TABLE IF NOT EXISTS jobs (
+            id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            job_type        TEXT NOT NULL,
+            status          TEXT NOT NULL DEFAULT 'pending'
+        )
+    ");
+    match res {
+        Ok(_) => {
+            let mut i = 0;
+            loop {
+                i += 1;
+                println!("Waiting for jobs... {}", i);
+            }
+        },
+        Err(e) => {
+            eprintln!("Database error: {}", e);
+            return Err(e);
+        }
+    }
 }
